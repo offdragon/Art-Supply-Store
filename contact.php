2f210b1ed0e6dc88e12166e243935e6cdcf92aa4
Robin Balatbat <64383337+offdragon@users.noreply.github.com>
2021-09-14 10:45:21 -0700

    Initial Whiteboxing
    Initial whiteboxing for contact and home page

---
+++ contact.php
@@ -0,0 +1,29 @@
+<!-- Contact Us Page -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us</title>
+</head>
+<body>
+    <?php include("include/header.php");?>
+    <!-- Main Body include:
+        Side bar with Customer Service
+            Contact Us
+            Shipping and Returns
+            FAQ
+        
+        Contact Us Body:
+            Email Us: *Make sure to send to SQL Server*
+                First name, Last name
+                Email, Phone (optional)
+                Subject Drop Down (options to select)
+                Message
+                Order Number
+                Send
+            Call or Mail
+    -->
+</body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,9 @@
+<!-- Header code -->
+<!-- Header Goes here include:
+        Logo
+        Search Bar
+        Sign-In
+        Cart
+        Navbar * Separate Php Include
+        *MAKE THIS ALL PHP INCLUDE*
+    -->
